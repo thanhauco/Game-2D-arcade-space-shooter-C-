1b5cfb569f1c92201811d62c1b09cf669326f0b2
Thanh Vu <thanhauco@gmail.com>
2019-03-20 10:42:18 -0700

    feat: add Enemy class header with enemy types

---
+++ Enemy.h
@@ -0,0 +1,44 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include "Entity.h"
+
+class Game;
+
+enum class EnemyType {
+  Drifter, // Floats down, fires occasionally
+  Hunter,  // Tracks player, aggressive
+  Bomber   // Large, drops cluster bombs
+};
+
+class Enemy : public Entity {
+public:
+  Enemy(float x, float y, EnemyType type);
+
+  void update(float deltaTime, Game &game);
+  void render(SDL_Renderer *renderer) override;
+
+  void takeDamage(int amount);
+
+  int getScoreValue() const { return scoreValue; }
+  EnemyType getType() const { return type; }
+
+private:
+  void updateDrifter(float deltaTime, Game &game);
+  void updateHunter(float deltaTime, Game &game);
+  void updateBomber(float deltaTime, Game &game);
+
+  void renderDrifter(SDL_Renderer *renderer);
+  void renderHunter(SDL_Renderer *renderer);
+  void renderBomber(SDL_Renderer *renderer);
+
+  EnemyType type;
+  int health;
+  int maxHealth;
+  int scoreValue;
+  float shootTimer;
+  float shootCooldown;
+  float animTimer;
+};
+
+#endif // ENEMY_H
